a7dafa5b36887246abadb9e3983e303ee489cfc1
nohsoobin <nohsoobin18@naver.com>
2022-11-29 12:07:05 +0900

    qwe

--- models/User.js
+++ models/User.js
@@ -1,23 +1,16 @@
 import mongoose from 'mongoose'
 
-const productSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    slug: { type: String, required: true, unique: true },
-    category: { type: String, required: true },
-    image: { type: String, required: true },
-    price: { type: Number, required: true },
-    brand: { type: String, required: true },
-    rating: { type: Number, required: true, default: 0 },
-    numReviews: { type: Number, required: true, default: 0 },
-    countInStock: { type: Number, required: true, default: 0 },
-    description: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
   },
   {
     timestamps: true,
   }
 )
 
-const Product =
-  mongoose.models.Product || mongoose.model('Product', productSchema)
-export default Product
+const User = mongoose.models.User || mongoose.model('User', userSchema)
+export default User
